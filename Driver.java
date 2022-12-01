07d9ed296d89d51cf1d39675a0f57f0a742f9e67
Sam Parkinson <43306113+sam-parkinson@users.noreply.github.com>
2022-11-30 22:34:52 -0500

    Initial commit

---
+++ Driver.java
@@ -0,0 +1,11 @@
+public class Driver {
+    public static void main(String[] args) {
+
+
+        System.out.println("Advent of Code 2022");
+
+        System.out.println();
+        System.out.println("Problem 1.1: ");
+        System.out.println("Problem 1.2: ");
+    }
+}
